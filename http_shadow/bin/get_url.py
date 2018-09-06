6c3215dccd7c4b496f967ea0fc172fb952a1bb7a
macbre <maciej.brencz@gmail.com>
2018-09-06 11:58:53 +0200

    Add get_url binary

---
+++ http_shadow/bin/get_url.py
@@ -0,0 +1,49 @@
+import logging
+from time import sleep
+
+from wikia_common_kibana import Kibana
+
+logging.basicConfig(level=logging.INFO)
+
+
+class AccessLog(object):
+
+    # how often to query elasticsearch for new access log entries
+    INTERVAL = 5
+
+    # query to use when taking URLs from access log
+    QUERY = 'verb: "GET"'
+
+    # which fields to fetch
+    FIELDS = ['hostname', 'request']
+
+    # how many URLs to fetch
+    BATCH = 150
+
+    def __init__(self):
+        self.kibana = Kibana(
+            period=self.INTERVAL,
+            index_prefix='logstash-apache-access-log'
+        )
+
+    @staticmethod
+    def format_log_entry(entry):
+        return 'http://{hostname}{request}'.format(**entry)
+
+    # yields URLs found in access log
+    def fetch(self):
+        while True:
+            res = self.kibana.query_by_string(self.QUERY, fields=self.FIELDS, limit=50)
+            urls = map(self.format_log_entry, res)
+
+            for url in urls:
+                yield url
+
+            sleep(self.INTERVAL)
+
+
+def main():
+    access_log = AccessLog()
+
+    for url in access_log.fetch():
+        print(url)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='shadow',
+    name='http_shadow',
     version='0.0.1',
     description='',
     url='https://github.com/macbre/http-shadow',
@@ -12,4 +12,9 @@ setup(
         'wikia-common-kibana==2.2.5',
     ],
     include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'get_urls=http_shadow.bin.get_url:main',  # query elasticsearch and emit found URLs to stdout
+        ],
+    }
 )
