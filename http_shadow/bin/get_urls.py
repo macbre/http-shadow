9a973fb6d8cf216b14a63b96e9b6421ec0fea34c
macbre <maciej.brencz@gmail.com>
2018-09-19 10:15:45 +0200

    Improve es querying over longer period

--- http_shadow/bin/get_urls.py
+++ http_shadow/bin/get_urls.py
@@ -8,6 +8,9 @@ class AccessLog(object):
     # how often to query elasticsearch for new access log entries
     INTERVAL = 10
 
+    # elasticsearch index with access log
+    ES_INDEX = 'logstash-apache-access-log'
+
     # query to use when taking URLs from access log
     QUERY = 'verb: "GET"'
 
@@ -17,12 +20,6 @@ class AccessLog(object):
     # how many URLs to fetch
     BATCH = 250
 
-    def __init__(self):
-        self.kibana = Kibana(
-            period=self.INTERVAL,
-            index_prefix='logstash-apache-access-log'
-        )
-
     @staticmethod
     def format_log_entry(entry):
         return 'http://{hostname}{request}'.format(**entry)
@@ -30,11 +27,12 @@ class AccessLog(object):
     # yields URLs found in access log
     def fetch(self):
         while True:
-            now = int(time.time())
-            self.kibana._since = now - self.INTERVAL + 1
-            self.kibana._to = now
+            es = Kibana(
+                period=self.INTERVAL,
+                index_prefix=self.ES_INDEX
+            )
 
-            res = self.kibana.query_by_string(self.QUERY, fields=self.FIELDS, limit=self.BATCH)
+            res = es.query_by_string(self.QUERY, fields=self.FIELDS, limit=self.BATCH)
             urls = map(self.format_log_entry, res)
 
             for url in urls:
